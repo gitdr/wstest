9b07c667e52cefa698ebc1b507bb52b39088f869
Dmitry Risics <dmitrijs.risics@vce.com>
2016-03-28 21:16:38 +0100

    working websockets + amqp

--- Gemfile
+++ Gemfile
@@ -5,3 +5,14 @@ gem 'puma'
 gem 'sinatra'
 gem 'faye-websocket'
 gem 'permessage_deflate'
+gem 'amqp'
+gem 'bunny'
+
+group :test do
+  gem 'rspec'
+end
+
+group :development do
+  gem 'rubocop'
+end
+
--- app.rb
+++ app.rb
@@ -1,15 +1,35 @@
 require './sapp'
 require 'faye/websocket'
+require 'eventmachine'
 require 'permessage_deflate'
+require 'amqp'
 require 'pp'
 
 options = {:extensions => [PermessageDeflate], :ping => 5}
 
 wapp = lambda do |env|
-
   if Faye::WebSocket.websocket?(env)
     ws = Faye::WebSocket.new(env, ['irc', 'xmpp'], options)
-    p [:open, ws.url, ws.version, ws.protocol]
+
+    connection = nil
+
+    begin
+      EM.schedule do
+        connection = AMQP.connect(:host => '192.168.1.85')
+        channel  = AMQP::Channel.new(connection)
+
+        queue    = channel.queue("amqpgem.examples.hello_world", :auto_delete => true)
+        exchange = channel.default_exchange
+
+        queue.subscribe do |payload|
+          ws.send(payload)
+        end
+      end
+    rescue ex
+      pp ex
+    end  
+
+    # p [:open, ws.url, ws.version, ws.protocol]
     ws.on :message do |event|
       ws.send(event.data)
     end
@@ -17,6 +37,8 @@ wapp = lambda do |env|
     ws.on :close do |event|
       p [:close, event.code, event.reason]
       ws = nil
+      connection.close
+      connection = nil
     end
 
     # Return async Rack response
@@ -34,11 +56,9 @@ App = Rack::Builder.new do
   # use Rack::CommonLogger
   # use Rack::ShowExceptions
   map "/" do
-    # use Rack::Lint
     run SApp.new
   end
   map "/ws" do
-    # use Rack::Lint
     run wapp
   end
 end
